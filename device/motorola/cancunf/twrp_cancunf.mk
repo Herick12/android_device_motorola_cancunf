e443541d8267783f985a6f5e102b272cd577002b
Herick Luis <herickluis2009@gmail.com>
2026-05-31 14:47:27 -0300

    Initial TWRP device tree base for cancunf

---
+++ device/motorola/cancunf/twrp_cancunf.mk
@@ -0,0 +1,7 @@
+PRODUCT_DEVICE := cancunf
+PRODUCT_NAME := twrp_cancunf
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto G73
+PRODUCT_MANUFACTURER := motorola
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
