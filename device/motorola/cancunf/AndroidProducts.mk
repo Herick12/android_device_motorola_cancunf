1e8bc30727894ae4d16ec898cb371c139ff6d91b
root <root@localhost.localdomain>
2026-06-02 02:58:39 -0300

    device tree cancunf

---
+++ device/motorola/cancunf/AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_cancunf.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_cancunf-eng
---
+++ device/motorola/cancunf/twrp_cancunf.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_DEVICE := cancunf
+PRODUCT_NAME := twrp_cancunf
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto G73
+PRODUCT_MANUFACTURER := motorola
